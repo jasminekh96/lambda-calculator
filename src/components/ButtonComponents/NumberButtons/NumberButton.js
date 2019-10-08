ba21b7213cf9efea855b03ae254f9a4cbb1b024a
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-10-08 19:00:08 -0400

    MVP

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
+import { tsPropertySignature } from '@babel/types';
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const NumberButton = (props) => {
+	return (
+		<button className='numbers'>
+			{props.button}
+			{/* Display a button element rendering the data being passed down from the parent container on props */}
+		</button>
+	);
 };
+export default NumberButton;
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import { tsPropertySignature } from '@babel/types';
 
-const OperatorButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const OperatorButton = (props) => {
+	return (
+		<button className='operators'>
+			{
+				props.data
+				/* Display a button element rendering the data being passed down from the parent container on props */
+			}
+		</button>
+	);
 };
+export default OperatorButton;
